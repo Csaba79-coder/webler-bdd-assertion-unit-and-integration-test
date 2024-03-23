3cadfb6237b174ca6bb26e50bbca5809e4bb4bc1
Csaba Vadász <csabavadasz79@gmail.com>
2024-03-23 15:57:21 +0100

    create Validator in util package (for email and password using regex pattern)

---
+++ src/main/java/hu/webler/weblerbddassertionunitandintegrationtest/util/Validator.java
@@ -0,0 +1,20 @@
+package hu.webler.weblerbddassertionunitandintegrationtest.util;
+
+import java.util.regex.Pattern;
+
+public class Validator {
+
+    public static boolean patternMatches(String pattern, String regexPattern) {
+        if (pattern == null) {
+            return false;
+        }
+        return Pattern
+                .compile(regexPattern)
+                .matcher(pattern)
+                .matches();
+    }
+
+    private Validator() {
+
+    }
+}
