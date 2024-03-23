8d0e60c07d2da4ef82631f1c1494ca29a5b9d887
Csaba Vadász <csabavadasz79@gmail.com>
2024-03-23 15:56:51 +0100

    update test with all necessary validation checking

--- src/test/java/hu/webler/weblerbddassertionunitandintegrationtest/util/ValidatorTest.java
+++ src/test/java/hu/webler/weblerbddassertionunitandintegrationtest/util/ValidatorTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Validator util - unit test")
 class ValidatorTest {
 
     private final static String EMAIL_VALIDATOR_PATTERN = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+    private final static String PASSWORD_VALIDATOR_PATTERN
+            = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!.?])(?=\\S+$).{8,20}$";
 
     @Test
     @DisplayName("Given valid email pattern and regex pattern when matching pattern returns true")
@@ -29,6 +32,7 @@ class ValidatorTest {
         then(isValidUkEmail).isTrue();
         then(isValidHuEmail).isTrue();
         assertThat(isMatching).isTrue();
+        assertTrue(isMatching);
     }
 
     @Test
@@ -62,6 +66,49 @@ class ValidatorTest {
         for (String email : invalidEmails) {
             boolean isMatching = Validator.patternMatches(email, EMAIL_VALIDATOR_PATTERN);
             then(isMatching).isFalse();
+        }
+    }
+
+    @Test
+    @DisplayName("Given valid password pattern and regex pattern when matching pattern then returns true")
+    public void givenValidPasswordPattern_whenMatchingPattern_thenReturnTrue() {
+        // Given
+        String[] validPasswords = {
+                "Almafa12134?",
+                "?Almafa1234",
+                "almaFa12@34",
+                "1234Almafa!!"
+        };
+
+        // When / Then
+        for (String password : validPasswords) {
+            boolean isMatching = Validator.patternMatches(password, PASSWORD_VALIDATOR_PATTERN);
+            then(isMatching).isTrue();
+            assertTrue(isMatching);
+        }
+    }
+
+    @Test
+    @DisplayName("Given invalid password pattern and regex pattern when matching pattern then returns false")
+    public void givenInvalidPasswordPattern_whenMatchingRegexPattern_thenReturnFalse() {
+        // Given
+        String[] invalidPasswords = {
+                null,
+                "",
+                "password",
+                "pa12@H",
+                "Almafa1234",
+                "Abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+Abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+",
+                "abcdefg1!",
+                "ABCDEFG1!",
+                "Abcdefg!",
+                "Abcdefg1"
+        };
+
+        // When / Then
+        for (String password : invalidPasswords) {
+            boolean isValid = Validator.patternMatches(password, PASSWORD_VALIDATOR_PATTERN);
+            then(isValid).isFalse();
         }
     }
 }
